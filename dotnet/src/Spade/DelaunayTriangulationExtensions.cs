c59dc31d66fc622e9f264ac22dff0af490bb5e70
ApprenticeGC <apprentice@giantcroissant.com>
2025-11-26 11:19:10 +0800

    feat: add dotnet config and triangulation extensions
    - Add dotnet/.gitignore for build artifacts
    - Add nuget.config for package sources
    - Add DelaunayTriangulationExtensions with hull and locate helpers

---
+++ dotnet/src/Spade/DelaunayTriangulationExtensions.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Spade.Handles;
+using Spade.Primitives;
+using Spade.DCEL;
+
+namespace Spade;
+
+public static class DelaunayTriangulationExtensions
+{
+    public static BarycentricInterpolator<V, DE, UE, F, L> Barycentric<V, DE, UE, F, L>(
+        this DelaunayTriangulation<V, DE, UE, F, L> triangulation)
+        where V : IHasPosition<double>, new()
+        where DE : new()
+        where UE : new()
+        where F : new()
+        where L : IHintGenerator<double>, new()
+    {
+        return new BarycentricInterpolator<V, DE, UE, F, L>(triangulation);
+    }
+
+    public static NaturalNeighborInterpolator<V, DE, UE, F, L> NaturalNeighbor<V, DE, UE, F, L>(
+        this DelaunayTriangulation<V, DE, UE, F, L> triangulation)
+        where V : IHasPosition<double>, new()
+        where DE : new()
+        where UE : new()
+        where F : new()
+        where L : IHintGenerator<double>, new()
+    {
+        return new NaturalNeighborInterpolator<V, DE, UE, F, L>(triangulation);
+    }
+
+    public static bool TryIsInsideConvexHull<V, DE, UE, F, L>(
+        this DelaunayTriangulation<V, DE, UE, F, L> triangulation,
+        Point2<double> position,
+        out bool isInside)
+        where V : IHasPosition<double>, new()
+        where DE : new()
+        where UE : new()
+        where F : new()
+        where L : IHintGenerator<double>, new()
+    {
+        isInside = false;
+        if (triangulation.NumVertices < 3)
+        {
+            // Not a valid hull yet
+            return false;
+        }
+
+        var loc = triangulation.LocateWithHintOptionCore(position, null);
+        if (loc is PositionInTriangulation.OutsideOfConvexHull)
+        {
+            isInside = false;
+            return true;
+        }
+
+        isInside = true;
+        return true;
+    }
+
+    public static bool TryGetHullCenterPattern<V, DE, UE, F, L>(
+        this DelaunayTriangulation<V, DE, UE, F, L> triangulation,
+        out FixedVertexHandle center,
+        out List<FixedVertexHandle> hullRing)
+        where V : IHasPosition<double>, new()
+        where DE : new()
+        where UE : new()
+        where F : new()
+        where L : IHintGenerator<double>, new()
+    {
+        center = default;
+        hullRing = new List<FixedVertexHandle>();
+
+        if (triangulation.NumVertices < 3)
+        {
+            return false;
+        }
+
+        var outerFace = triangulation.OuterFace();
+        var adjacent = outerFace.AdjacentEdge();
+
+        if (adjacent == null)
+        {
+            return false;
+        }
+
+        var start = adjacent.Value;
+        var current = start;
+        var visited = new HashSet<int>();
+
+        do
+        {
+            if (!visited.Add(current.Handle.Index))
+            {
+                // Cycle detected in outer face traversal (should be a loop anyway)
+                break;
+            }
+            hullRing.Add(current.From().Handle);
+            current = current.Next();
+        } while (current.Handle != start.Handle);
+
+        if (triangulation.NumVertices != hullRing.Count + 1)
+        {
+            return false;
+        }
+
+        var hullIndices = new HashSet<int>();
+        foreach (var v in hullRing)
+        {
+            hullIndices.Add(v.Index);
+        }
+
+        for (int i = 0; i < triangulation.NumVertices; i++)
+        {
+            if (!hullIndices.Contains(i))
+            {
+                center = new FixedVertexHandle(i);
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
